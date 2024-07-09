860a113a51421ab9de7d18ee4e2f62f63c1ba98e
Ital02 <devitalomiranda@gmail.com>
2024-07-09 19:38:54 -0300

    feat: adding initial files

---
+++ src/main/java/io/github/ital023/agregadoinvestimentos/AgregadoinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.agregadoinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadoinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadoinvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/agregadoinvestimentos/AgregadoinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.agregadoinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadoinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
